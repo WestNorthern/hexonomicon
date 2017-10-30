c8d5b847477ac414d8ffc200e7887ca14ce45f40
westnorthern <rrichardson610@gmail.com>
2017-10-30 14:11:47 -0600

    fixed the nav bar to highlight current page

--- main.rb
+++ main.rb
@@ -2,10 +2,12 @@ require 'sinatra'
 require 'pony'
 
 get '/' do
+	params[:controller] = 'home'
 	erb :home
 end
 
 get '/log_in' do
+	params[:controller] = 'login'
 	erb :log_in
 end
 
@@ -15,11 +17,13 @@ post '/log_in' do
 	@last_name = params[:last_name]
 	@email = params[:email]
 	@password = params[:password]
+	params[:controller] = 'home'
 	erb :home
 end
 
 
 get '/services' do
+	params[:controller] = 'services'
 	erb :services
 end
 		
@@ -28,6 +32,7 @@ get '/cart' do
 end
 	
 get '/contact' do
+	params[:controller] = 'contact'
 	erb :contact
 end
 
@@ -35,10 +40,11 @@ post '/contact' do
 	@email = params[:email]
 	@message = params[:message]
 	Pony.mail(:to => 'rrichardson610@gmail.com', :from => @email, :subject => 'Contact Us', :body => @message)
-
+	params[:controller] = 'contact'
 	erb :contact_conf
 end
 
 get '/about' do
+	params[:controller] = 'about'
 	erb :about
 end
